24b9ad5dc25a5b3bef975fd2bb72ce174228ef72
Ayan Sinha <sinhaayan@yahoo.com>
2020-12-29 16:30:38 +0530

    Row Component, axios added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Row from './Components/Row';
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
       <h1>Movie DB</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Tranding Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
---
+++ src/Components/Row.js
@@ -0,0 +1,38 @@
+import axios from '../axios';
+import React, { useEffect, useState } from 'react';
+import './row.css';
+
+
+function Row({ title, fetchUrl }) {
+
+    const base_URL = "https://image.tmdb.org/t/p/original/";
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.log(movies);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row_posters">
+                {movies.map(item => (
+                    <img className="row_poster"
+                        src={`${base_URL}${item.poster_path}`}
+                        alt={item.name} onClick={() => {
+                            console.log("movie ID:>>>>> ", item.id);
+                        }} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Row;
